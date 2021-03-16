321c9984a4da2f80c6a7d4e4823cd4264872f753
Weibienaole <weibienaole@163.com>
2021-03-16 17:35:38 +0800

    3-16updates

--- React-demo-project-mobile/src/components/topBar/index.jsx
+++ React-demo-project-mobile/src/components/topBar/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-
+import AppMethods from '../../utils/appInteractive'
 import './index.css'
 
 /*
@@ -9,6 +9,12 @@ import './index.css'
       0 - 黑色主题
       1 - 白色
     title  标题
+    app 
+      true - app交互事件进行返回
+      false - 页面内返回
+    noArrow
+      true - 隐藏箭头
+      false - 显示
 */
 class TopBar extends Component {
   constructor(props) {
@@ -16,14 +22,15 @@ class TopBar extends Component {
     this.state = {}
   }
   render() {
+    console.log(this.props.app, 'app');
     return (
       <div className="topBar-component">
-        <img
+        {!this.props.noArrow && <img
           src={require(`${this.props.type === '0' ? './image/backArrow_black.png' : './image/backArrow.png'}`)}
           alt=""
           className="arrow"
-          onClick={() => this.props.history.goBack(-1)}
-        />
+          onClick={() => this.props.app ? AppMethods('closePage') : this.props.history.goBack(-1)}
+        />}
         <span className="topBarTil" style={{color: this.props.type === '0' ? '#000' : '#fff'}}>{this.props.title}</span>
       </div>
     )
