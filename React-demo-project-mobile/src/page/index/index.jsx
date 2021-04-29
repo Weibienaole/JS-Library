e665a5df43e7c93937d4124c832aba5f3ac4d56c
Weibienaole <weibienaole@163.com>
2021-04-29 18:06:19 +0800

    4-29_react_mobile-updates

--- React-demo-project-mobile/src/page/index/index.jsx
+++ React-demo-project-mobile/src/page/index/index.jsx
@@ -17,7 +17,6 @@ class Page extends Component {
       'http://localhost:3000/?token=0ffecc644c133f97367dc7f8f1e7a72da5161d1e958af2b0e2a7f22329a04eb777f71cd2038e2e01532424ba629f8460435122bdb2990ca1a6f57cc630099f4300fbf0f2a085eb38d200108ec92783b0c6d1ca2438b7f4e8dd6f1e152add18d96b1e822336348e5df24875f77ce09bfe9ab03077d5c64652b0ef35926565b5f0b7940658857cef6c'
     let { token } = devtools.getUrlData(url)
     console.log(token)
-    console.log(process.env.REACT_APP_ENV, 'huanj');
   }
   render() {
     return <div className="page-wrap">demo</div>
